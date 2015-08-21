f891aa10cc4086ea53cb2bfd1a4269966890ae37
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2015-08-21 18:00:39 +0300

    Imagine that Gear is part of a framework and that its initialization method requires fixed-order arguments

--- dependences/gear.rb
+++ dependences/gear.rb
@@ -1,53 +1,69 @@
-class Gear
+module SomeFramework
 
-  attr_reader :chainring, :cog, :wheel
+  class Gear
 
-  def initialize(args)
-    args = defaults.merge(args)
-    @chainring = args.fetch(:chainring, 40)
-    @cog       = args.fetch(:cog, 18)
-    @wheel     = args[:wheel]
+    attr_reader :chainring, :cog, :wheel
+
+    # def initialize(args)
+    #   args = defaults.merge(args)
+    #   @chainring = args[:chainring]
+    #   @cog       = args[:cog]
+    #   @wheel     = args[:wheel]
+    # end
+
+    # def defaults
+    #   {:chainring => 40, :cog => 18}
+    # end
+
+    def initialize(chainring, cog, wheel)
+      @chainring = chainring
+      @cog       = cog
+      @wheel     = wheel
+    end
+
+    def ratio
+      chainring / cog.to_f
+    end
+
+    def gear_inches
+      ratio * diameter
+    end 
+
+    def diameter
+      wheel.diameter
+    end
+
   end
-
-  def defaults
-    {:chainring => 40, :cog => 18}
-  end
-
-  def ratio
-    chainring / cog.to_f
-  end
-
-  def gear_inches
-    ratio * diameter
-  end 
-
-  def diameter
-    wheel.diameter
-  end
-
 end
 
-class Wheel
+ class Wheel
 
-  attr_reader :rim, :tire
+    attr_reader :rim, :tire
 
-  def initialize(rim, tire) 
-    @rim = rim
-    @tire = tire
+    def initialize(rim, tire) 
+      @rim = rim
+      @tire = tire
+    end
+
+    def diameter
+      rim + (tire * 2)
+    end
+
+    def circumference 
+      diameter * Math::PI
+    end 
+
   end
-
-  def diameter
-    rim + (tire * 2)
+  
+module GearWrapper
+  def self.gear(args)
+      SomeFramework::Gear.new(args[:chainring],
+                              args[:cog],
+                              args[:wheel])
   end
-
-  def circumference 
-    diameter * Math::PI
-  end 
-
 end
 
-
-puts Gear.new(
+GearWrapper.gear(
   :chainring => 52,
   :cog       => 11,
   :wheel     => Wheel.new(26, 1.5)).gear_inches
