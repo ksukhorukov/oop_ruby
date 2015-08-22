f68d3c73a7b418f7a8800719ac0bd55b03ca8885
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2015-08-22 09:23:03 +0300

    added bicycle class

--- inheritance/inheritance.rb
+++ inheritance/inheritance.rb
@@ -1,23 +1,32 @@
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
 
   def initialize(args)
     @size =  args[:size]
     @tape_color = args[:tape_color]
+    @tape_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
 
   #every bike has the same defaults
   #for tire and chain size 
 
+  # checking "style" starts down a slippery slope
   def spares
-    { chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color }
+    if style == :road 
+      { chain: '10-speed',
+        tire_size: '23',
+        tape_color: tape_color }
+    else
+          { chain: '10-speed',
+        tire_size: '23',
+        rear_shock: rear_shock } 
   end
 
   #Many other methods
 end
 
-bike = Bicycle.new( size: 'M', tape_caolor: 'red')
+bike = Bicycle.new(  style: :mountain, size: 'S', front_shock: 'Manitou', rear_shock: 'Fox')
 bike.size 
 bike.spares
